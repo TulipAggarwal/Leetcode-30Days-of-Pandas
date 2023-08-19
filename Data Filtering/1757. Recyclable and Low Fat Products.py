a5eb76b62446358f71199d83dce946d7a2b8f906
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-08-19 22:27:45 +0530

    Create 1757. Recyclable and Low Fat Products.py

---
+++ Data Filtering/1757. Recyclable and Low Fat Products.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return df[['product_id']]
