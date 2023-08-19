d491d875ffac676cc2e4405383fa08def523975c
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-08-19 22:23:35 +0530

    Create 595. Big Countries.py

---
+++ Data Filtering/595. Big Countries.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+        df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+        return df[['name','population','area']]
